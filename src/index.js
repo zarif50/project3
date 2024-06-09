4a518f64db0d6243cfe1428c32153eb5e0a63c21
zarif50 <ans123@gmail.com>
2024-06-09 23:17:26 +0600

    home page done with html and css Day2 10/06/24

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.jsx';
+import reportWebVitals from './reportWebVitals.js';
+import {BrowserRouter} from 'react-router-dom'
+import StoreContextProvider from './context/StoreContext.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <BrowserRouter>
+  <StoreContextProvider>
+    
+          <App/>
+
+
+  </StoreContextProvider>
+
+            
+            
+  </BrowserRouter>
+
+ 
+)
