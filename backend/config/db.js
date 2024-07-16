5908e0b99e071d99d4cd4865e4ca2d335a3ad43c
Muhammed Tareq Aziz <muhammedtareqaziz@Muhammeds-MacBook-Pro.local>
2024-07-16 23:59:15 +0600

    file folder restructured

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,10 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://tanimtanzilislam9:16137025081203@cluster0.qr0qwa5.mongodb.net/FOOD-APPmon?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"));
-}
-
-  
-  
-  
-  
+  await mongoose.connect(process.env.MONGO_URI).then(() => console.log("DB Connected"));
+};
 
 //
-
-
 
 //mongodb+srv://tanimtanzilislam9:16137025081203@cluster0.qr0qwa5.mongodb.net/FOOD-APP
 
@@ -19,12 +12,4 @@ export const connectDB = async () => {
 
 //mongodb+srv://tanimtanzilislam9:<password>@cluster0.qr0qwa5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
-
-
-
-
 //mongodb+srv://vercel-admin-user:<password>@cluster0.qr0qwa5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-
-
-
-
---
+++ frontend/src/context/StoreContext.jsx
@@ -0,0 +1,62 @@
+import { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/assets";
+import axios from "axios";
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+  const [token, setToken] = useState("");
+
+  const addToCart = async (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+    if (token) {
+      await axios.post(`${process.env.REACT_APP_API_URL}/api/cart/add`, { itemId }, { headers: { token } });
+    }
+  };
+
+  const removeFromCart = async (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if (token) {
+      await axios.post(`${process.env.REACT_APP_API_URL}/api/cart/remove`, { itemId }, { headers: { token } });
+    }
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        if (itemInfo) {
+          totalAmount += itemInfo.price * cartItems[item];
+        }
+      }
+    }
+    return totalAmount;
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
+    }
+  }, []);
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount,
+    url,
+    token,
+    setToken,
+  };
+
+  return <StoreContext.Provider value={contextValue}>{props.children}</StoreContext.Provider>;
+};
+
+export default StoreContextProvider;
