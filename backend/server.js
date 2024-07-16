83b7fb2c78a11e1667987f67027e6c8f219e0696
Muhammed Tareq Aziz <muhammedtareqaziz@Muhammeds-MacBook-Pro.local>
2024-07-17 00:04:42 +0600

    fs removed

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 
 //api endpoints
 app.use("/api/food", foodRouter);
-app.use("/images", express.static("uploads"));
+// app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
 
